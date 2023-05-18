f6d4c21f04e24588b9d775b1c935cdaab86bd348
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-05-18 13:10:23 -0300

    feat: add Chat Area Component

---
+++ src/components/ChatArea.tsx
@@ -0,0 +1,20 @@
+import ChatMessageItem from './ChatMessageItem'
+import ChatPlaceholder from './ChatPlaceholder'
+
+import Chat from '@/types/Chat'
+
+interface Props {
+	chat: Chat | undefined 
+}
+
+export default function ChatArea({ chat }: Props) {
+	return (
+		<main className="overflow-y-scroll">
+			{!chat && <ChatPlaceholder />}
+
+			{chat && chat.messages.map(message => (
+				<ChatMessageItem key={message.id} message={message} />
+			))}
+		</main>
+	)
+}
