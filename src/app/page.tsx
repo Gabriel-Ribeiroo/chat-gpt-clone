843205362954ce2b8f71c1505a90da3f0b5e3993
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-05-18 13:11:02 -0300

    feat: add Chat Placeholder Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,22 @@
 
 import { useState } from 'react'
 
-import Sidebar from '@/components/Sidebar'
 import Header from '@/components/Header'
+import ChatArea from '@/components/ChatArea'
+import Sidebar from '@/components/Sidebar'
+
+import Chat from '@/types/Chat'
 
 export default function Home() {
   const [isSidebarOpened, setIsSidebarOpened] = useState(false)
+	const [currentChat, setCurrentChat] = useState<Chat>({
+		id: '123',
+		title: 'Bla blu',
+		messages: [
+			{ id: '99', author: 'me', body: 'Eai' },
+			{ id: '99', author: 'ai', body: 'Olá! como posso te ajudar?' },
+		]
+	})
 
 	const handleCloseSidebarClick = () => setIsSidebarOpened(false)
 	
@@ -20,6 +31,8 @@ export default function Home() {
 
 			<div className="flex flex-col w-full">
 				<Header handleOpenSidebarClick={handleOpenSidebarClick} />
+
+				<ChatArea chat={currentChat} />
 			</div>
 			
 		</div>
---
+++ src/components/ChatPlaceholder.tsx
@@ -0,0 +1,76 @@
+import SunIcon from './icons/SunIcon'
+
+export default function ChatPlaceholder() {
+	return (
+		<div className="m-5 text-white">
+			<h1 className="text-4xl font-bold text-center my-8">ChatGPT</h1>
+
+			<div className="flex flex-col md:flex-row gap-5 mb-8 m-auto md:max-w-4xl">
+
+				<div className="flex flex-col gap-5">
+
+					<div className="flex gap-5 justify-center">
+						<SunIcon width={24} height={24} />
+						<p>Exemplo</p>
+					</div>
+
+					<div className="bg-white/5 rounded p-3 text-center text-sm">
+						<p>"Explique o sentido da vida em termos simples"</p>
+					</div>
+					
+					<div className="bg-white/5 rounded p-3 text-center text-sm">
+						<p>"Me de ideias criativas para um aniversário de 10 anos"</p>
+					</div>
+
+					<div className="bg-white/5 rounded p-3 text-center text-sm">
+						<p>"Como eu faço uma requisição HTTP em JavaScript?"</p>
+					</div>
+
+				</div>
+
+				<div className="flex flex-col gap-5">
+
+					<div className="flex gap-5 justify-center">
+						<SunIcon width={24} height={24} />
+						<p>Capacidades</p>
+					</div>
+
+					<div className="bg-white/5 rounded p-3 text-center text-sm">
+						<p>"Explique o sentido da vida em termos simples"</p>
+					</div>
+					
+					<div className="bg-white/5 rounded p-3 text-center text-sm">
+						<p>"Me de ideias criativas para um aniversário de 10 anos"</p>
+					</div>
+
+					<div className="bg-white/5 rounded p-3 text-center text-sm">
+						<p>"Como eu faço uma requisição HTTP em JavaScript?"</p>
+					</div>
+
+				</div>
+
+				<div className="flex flex-col gap-5">
+
+					<div className="flex gap-5 justify-center">
+						<SunIcon width={24} height={24} />
+						<p>Limitações</p>
+					</div>
+
+					<div className="bg-white/5 rounded p-3 text-center text-sm">
+						<p>"Explique o sentido da vida em termos simples"</p>
+					</div>
+					
+					<div className="bg-white/5 rounded p-3 text-center text-sm">
+						<p>"Me de ideias criativas para um aniversário de 10 anos"</p>
+					</div>
+
+					<div className="bg-white/5 rounded p-3 text-center text-sm">
+						<p>"Como eu faço uma requisição HTTP em JavaScript?"</p>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+	)
+}
