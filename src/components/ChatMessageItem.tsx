3afdf2ddd79e9d2243e99b149d5e857ce33d1b2a
Gabriel Ribeiro <gabriel18ribeiro2012@gmail.com>
2023-05-19 18:14:56 -0300

    style: change user/gpt icon width and height

--- src/components/ChatMessageItem.tsx
+++ src/components/ChatMessageItem.tsx
@@ -8,13 +8,13 @@ interface Props {
 
 export default function ChatMessageItem({ message }: Props) {
 	return (
-		<div className={`py-7 text-white ${message.author === 'ai' && 'bg-gray-600/50'}`}>
+		<div className={`py-7 px-4 text-white ${message.author === 'ai' && 'bg-gray-600/50'}`}>
 
 			<div className="flex gap-5 max-w-4xl m-auto items-center ">
 
 				<div 
-					className={`w-8 h-8 flex justify-center items-center rounded
-					ml-4 ${message.author === 'me' ? 'bg-blue-900' : 'bg-green-500/75'}`}
+					className={`w-10 h-10 flex justify-center items-center rounded
+					${message.author === 'me' ? 'bg-blue-900' : 'bg-green-500/75'}`}
 				>
 					{message.author === 'me' && <UserIcon width={24} height={24} />}
 					{message.author === 'ai' && <GPTIcon />}
